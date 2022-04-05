4687d23f62b92b55465af4af8f67ba1fe5240be9
J S Packiaraj <jsp@jsp.net.in>
2022-04-05 08:57:02 +0530

    FIles Handling in Java

--- 20220405/untitled/src/FirstFile.java
+++ 20220405/untitled/src/FirstFile.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class FirstFile {
+import java.io.File;
+public class FirstFile {
+    public static void main(String[] args) {
+        // Create a file called TG2_NewFile.txt
+        // in the directory C:\JAVA_Files\JAVAFilesDemo\
+        // (Change the directory as per your machine's directory structure
+        try{
+            File fsFileObject = new File("C:\\JAVA_Files\\JAVAFilesDemo\\TG2_NewFile.txt");
+            if(fsFileObject.)
+        }
+    }
 }
